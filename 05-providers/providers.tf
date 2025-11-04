fd63e7d828b0650e2419b8f2945a9453a269f2a0
shubheshswain91 <shubheshswain91@gmail.com>
2025-11-04 10:27:20 +0100

    providers with alias

--- 05-providers/providers.tf
+++ 05-providers/providers.tf
@@ -1,27 +1,27 @@
 terraform {
-  required_version = "~> 1.7"
+  required_version = "~> 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-}
 
-provider "aws" {
-  region = "eu-west-1"
 }
-
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "aws" {
+  region = "us-east-2"
   alias  = "us-east"
 }
 
-resource "aws_s3_bucket" "eu_west_1" {
-  bucket = "some-random-bucket-name-aosdhfoadhfu"
+resource "aws_s3_bucket" "us_west_1" {
+  bucket = "some-random-bucket-name-asdfgghhh"
 }
 
-resource "aws_s3_bucket" "us_east_1" {
-  bucket   = "some-random-bucket-name-18736481364"
+resource "aws_s3_bucket" "us_east_2" {
+  bucket   = "some-random-bucket-name-asdf123dd"
   provider = aws.us-east
 }
